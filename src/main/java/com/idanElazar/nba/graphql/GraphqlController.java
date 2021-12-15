d17a887199ecaa0ad53c1effd7720121cc33a5bd
idanela <58819088+idanela@users.noreply.github.com>
2021-12-15 11:35:32 +0200

    decided to add non-rest aspect

---
+++ src/main/java/com/idanElazar/nba/graphql/GraphqlController.java
@@ -0,0 +1,52 @@
+package com.idanElazar.nba.graphql;
+
+import com.idanElazar.nba.club.Club;
+import com.idanElazar.nba.player.BasketballPlayer;
+import com.idanElazar.nba.player.PlayerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@Controller
+public class GraphqlController {
+
+    private final PlayerService playerService;
+
+    @Autowired
+    public GraphqlController(PlayerService playerService) {
+        this.playerService = playerService;
+    }
+
+    @QueryMapping
+    List<BasketballPlayer> players()
+    {
+        return playerService.getPlayersList() ;
+    }
+
+    @QueryMapping
+    BasketballPlayer playerByName(@Argument String name)
+    {
+        return playerService.getPlayer(name);
+    }
+
+    @MutationMapping
+   String changeWeight (@Argument String name,@Argument  String weight)
+    {
+        BasketballPlayer player = playerService.getPlayer(name);
+        Integer newWeight = Integer.valueOf(player.getWeight())+ Integer.valueOf(weight);
+        playerService.updatePlayer(player.getName(),null,newWeight.toString(),null,null,null,null);
+        return newWeight.toString();
+    }
+
+
+
+
+
+
+
+
+}
